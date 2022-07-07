45a0edb092c23b641481eaeb1e488ba3807d28ee
distributive <github@ameliewd.com>
2022-07-07 11:13:18 +0100

    Stuff

--- src/ChefBot/Plugins/Recipe.hs
+++ src/ChefBot/Plugins/Recipe.hs
@@ -9,29 +9,43 @@
 -- A command that loads recipes.
 module ChefBot.Plugins.Recipe (recipePlugin) where
 
-import Control.Monad.IO.Class (MonadIO (liftIO))
-import Data.Text (pack)
+import Control.Monad.Reader (ask, liftIO)
+import Data.Aeson (FromJSON)
+import Data.Yaml (decodeFileEither)
+import Data.Yaml.Internal (ParseException)
+import Data.Text (Text, intercalate, unpack)
 import ChefBot.Utility
-import ChefBot.Utility.Discord (Message, formatFromEmojiName, sendEmbedMessage)
-import ChefBot.Utility.Embed (basicEmbed)
-import ChefBot.Utility.Parser
-import ChefBot.Utility.Random (chooseOne)
-import ChefBot.Utility.SmartParser (PComm (parseComm))
-import Text.Megaparsec
+import ChefBot.Utility.Discord (sendEmbedMessage)
+import ChefBot.Utility.Embed (basicEmbed, addFooter)
+import ChefBot.Utility.SmartParser (PComm (parseComm), RestOfInput (ROI))
+import ChefBot.Utility.Search (closestValue)
 import Text.RawString.QQ
+import GHC.Generics (Generic)
+import Discord.Internal.Types
 
 -- | @recipe@.
-recipe :: Command
-recipe = Command "recipe" recipecomm []
+recipe :: EnvCommand CookBook
+recipe = Command "recipe" (parseComm recipeComm) []
   where
-    recipecomm :: Parser (Message -> DatabaseDiscord ())
-    recipecomm = do
-      args <- (try quoted <|> nonSpaceWord) `sepBy` some space
-      return $ \m -> do
-        c <- case length args of
-          0 -> liftIO $ chooseOne [":arrow_up: Heads", ":arrow_down: Tails"]
-          _ -> liftIO $ chooseOne $ map (\a -> "> " <> pack a) args
-        sendEmbedMessage m "" $ basicEmbed ":game_die: Result :game_die:" c
+    recipeComm :: RestOfInput Text -> Message -> EnvDatabaseDiscord CookBook ()
+    recipeComm (ROI q) m = do
+      book <- ask
+      let res = closestValue (map (\r -> (unpack $ title r, r)) $ recipes book) (unpack q)
+      case q of
+        "" -> sendEmbedMessage m "" $ basicEmbed (":scroll: **All Recipes**") ("Try `recipe <recipe>` to view one of these:\n" <> allRecipes book)
+        _ -> sendEmbedMessage m "" $ embedRecipe res
+    allRecipes :: CookBook -> Text
+    allRecipes book = intercalate "\n" $ map (\r -> "• " <> title r) $ recipes book
+    embedRecipe :: Recipe -> Embed
+    embedRecipe res =
+      let header = ":page_with_curl: **" <> title res <> "**"
+          ings = case ingredients res of
+            [] -> ""
+            _ -> "**Ingredients**\n" <> (intercalate "\n" $ map (\i -> "• " <> i) $ ingredients res) <> "\n\n"
+          meth = "**Method**\n" <> method res
+          body = ings <> meth
+          footer = "Recipe provided by " <> citation res
+      in addFooter footer $ basicEmbed header body
 
 recipeHelp :: HelpPage
 recipeHelp =
@@ -39,14 +53,49 @@ recipeHelp =
     "recipe"
     []
     "displays a recipe"
-    [r|Randomly picks one element from its arguments or, if none are provided, either 'Heads' or 'Tails'.
+    [r|Displays a given recipe, or lists all the available recipes if none provided.
 
 **Usage**
-`flip` outputs 'Heads' or 'Tails' with uniform probability
-`flip a b c` outputs 'a', 'b', or 'c' with uniform probability|]
+`recipe` displays a list of all recipes.
+`recipe <recipe>` displays the given recipe.|]
     []
     None
 
+-- | @Recipe@ represents a single recipe.
+data Recipe = Recipe
+  { title :: Text,
+    ingredients :: [Text],
+    method :: Text,
+    citation :: Text
+  }
+  deriving (Eq, Show, Generic)
+instance FromJSON Recipe
+
+-- | @CookBook@ is the type representing all recipes.
+data CookBook = CookBook
+  { recipes :: [Recipe]
+  }
+  deriving (Eq, Show, Generic)
+instance FromJSON CookBook
+
+defaultCookBook :: CookBook
+defaultCookBook = CookBook {recipes = []}
+
+-- | @getCookBook@ loads the recipes from file.
+getCookBook :: IO CookBook
+getCookBook = do
+  cookbook <- decodeFileEither yamlFile :: IO (Either ParseException CookBook)
+  return $ case cookbook of
+    Left _ -> defaultCookBook
+    Right out -> out
+  where
+    yamlFile :: FilePath
+    yamlFile = "resources/cookbook.yaml"
+
+-- | @recipeStartup@ loads the recipes once at start up.
+recipeStartup :: StartUp CookBook
+recipeStartup = StartUp $ liftIO getCookBook
+
 -- | @flipPlugin@ assembles the command into a plugin.
-recipePlugin :: Plugin
-recipePlugin = (plug "recipe") {commands = [recipe], helpPages = [recipeHelp]}
+recipePlugin :: EnvPlugin CookBook
+recipePlugin = (envPlug "recipe" recipeStartup) {commands = [recipe], helpPages = [recipeHelp]}
